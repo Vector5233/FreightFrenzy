550aa9aed3da9fa65a7c59df93c60da954cc8c65
Vector5233A <vector5233@gmail.com>
2021-11-02 11:42:33 -0400

    11/2 Class

--- drivingTeleOp.java
+++ drivingTeleOp.java
@@ -29,7 +29,7 @@ public class drivingTeleOp extends OpMode {
         leftDuckSpinner = hardwareMap.dcMotor.get("leftDuckSpinner");
         rightDuckSpinner = hardwareMap.dcMotor.get("rightDuckSpinner");
 
-        freightGrabber = hardwareMap.crservo.get("freightGrabber");
+        freightGrabber = (DcMotorEx) hardwareMap.dcMotor.get("freightGrabber");
 
         frontRight.setDirection(DcMotor.Direction.FORWARD);
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
