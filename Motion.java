644905b581c28ce6ecc004e98f35b78d42c8cc99
Vector5233A <vector5233@gmail.com>
2022-05-06 16:32:07 -0400

    Working on Motion class

--- Motion.java
+++ Motion.java
@@ -1,7 +1,60 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 class Motion {
-    public void driveStraight(double power, int ticks, boolean direcion) {
+
+    DcMotor FL, FR, BL, BR;
+
+    public final Boolean FORWARD = true;
+    public final Boolean BACKWARD = true;
+
+    public Motion() {
+        FL = hardwareMap.dcMotor.get("frontLeft");
+        FR = hardwareMap.dcMotor.get("frontRight");
+        BL = hardwareMap.dcMotor.get("backLeft");
+        BR = hardwareMap.dcMotor.get("backRight");
+        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        BR.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void setModeAll(DcMotor.RunMode mode) {
+        FL.setMode(mode);
+        FR.setMode(mode);
+        BL.setMode(mode);
+        BR.setMode(mode);
+    }
+
+    public void setPowerAll(double power) {
+        FL.setPower(power);
+        FR.setPower(power);
+        BL.setPower(power);
+        BR.setPower(power);
+    }
+
+
+    public void setTargetAll(int target) {
+        FL.setTargetPosition(target);
+        FR.setTargetPosition(target);
+        BL.setTargetPosition(target);
+        BR.setTargetPosition(target);
+    }
+
+    public void driveStraight(double power, int ticks, boolean direction) {
+        setModeAll(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setModeAll(DcMotor.RunMode.RUN_TO_POSITION);
+        setTargetAll(ticks);
+        setPowerAll(power);
+        while (FL.isBusy() && linearOpMode.opModeIsActive()) {
+        }
+        setPowerAll(0);
+        setModeAll(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
 
     }
 }
